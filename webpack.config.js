7b470569ed86fa19697fa6fc02b1bc1949001468
bhargav <bhargav.s127.0.0.1coder@gmail.com>
2024-05-18 00:46:58 +0530

    initial commit 🌟
    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // Other webpack configuration...
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
